dbc883d0171e53d38dd4e23b2908fa1ae1daf9d1
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-10-19 22:41:57 +0330

    Nothing

--- app/src/main/java/com/example/noteapps/HomeFragment.kt
+++ app/src/main/java/com/example/noteapps/HomeFragment.kt
@@ -1,11 +1,9 @@
 package com.example.noteapps
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.noteapps.databinding.FragmentHomeBinding
