fd74809a085e7f546375d393068d331caa5d39fb
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-10-19 22:41:44 +0330

    Create fragmentContainerView & set data binding

--- app/src/main/java/com/example/noteapps/MainActivity.kt
+++ app/src/main/java/com/example/noteapps/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.noteapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import com.example.noteapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
