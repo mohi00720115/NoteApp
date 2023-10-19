f1af595319abf305ba652da6d0a9e9d75a3c3564
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-10-19 22:19:13 +0330

    Create Home Fragment

---
+++ app/src/main/java/com/example/noteapps/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.noteapps
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.noteapps.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var navController: NavController
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DataBindingUtil.bind(view)!!
+        navController = findNavController()
+    }
+
+}
