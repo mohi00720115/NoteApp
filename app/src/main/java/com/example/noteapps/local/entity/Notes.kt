a6f985adaaec2bf8f89c04a084d98484821def9e
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-08 18:26:30 +0330

    Edit Notes.kt

--- app/src/main/java/com/example/noteapps/local/entity/Notes.kt
+++ app/src/main/java/com/example/noteapps/local/entity/Notes.kt
@@ -3,26 +3,33 @@ package com.example.noteapps.local.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(tableName = "notes")
-data class Notes(
+class Notes : Serializable {
     @PrimaryKey(autoGenerate = true)
-    var id: Int,
+    var id: Int? = null
+
     @ColumnInfo(name = "title")
-    var title: String,
+    var title: String? = null
+
     @ColumnInfo(name = "sub_title")
-    var subTitle: String,
+    var subTitle: String? = null
+
     @ColumnInfo(name = "date_time")
-    var dateTime: String,
+    var dateTime: String? = null
+
     @ColumnInfo(name = "note_text")
-    var noteText: String,
+    var noteText: String? = null
+
     @ColumnInfo(name = "img_path")
-    var imgPath: String,
+    var imgPath: String? = null
+
     @ColumnInfo(name = "web_link")
-    var webLink: String,
+    var webLink: String? = null
+
     @ColumnInfo(name = "color")
-    var color: String
-) {
+    var color: String? = null
 
     override fun toString(): String {
         return "$title : $dateTime"
