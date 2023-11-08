ca6a37c34eb3f45f76ed2b0760bfe3b735091605
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-08 18:27:32 +0330

    inflate layout

--- app/src/main/java/com/example/noteapps/HomeFragment.kt
+++ app/src/main/java/com/example/noteapps/HomeFragment.kt
@@ -1,17 +1,23 @@
 package com.example.noteapps
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.noteapps.databinding.FragmentHomeBinding
 
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : BaseFragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var navController: NavController
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DataBindingUtil.bind(view)!!
