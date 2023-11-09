aa1841ed0ba06b5efc5c5979d5ceea903e0a6107
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-09 20:52:17 +0330

    create NotesAdapter.kt

---
+++ app/src/main/java/com/example/noteapps/ui/adapter/NotesAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.noteapps.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.noteapps.databinding.ItemRvNoteBinding
+import com.example.noteapps.local.entity.Notes
+
+class NotesAdapter(private var arrList: List<Notes>) : RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesAdapter.NotesViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemRvNoteBinding.inflate(inflater, parent, false)
+        return NotesViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NotesAdapter.NotesViewHolder, position: Int) = holder.bindItem(arrList[position])
+
+    override fun getItemCount(): Int = arrList.size
+
+
+    class NotesViewHolder(private val binding: ItemRvNoteBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindItem(item: Notes) {
+            binding.apply {
+                tvTitle.text = item.title
+                tvDesc.text = item.noteText
+                tvDateTime.text = item.dateTime
+            }
+        }
+    }
+}
