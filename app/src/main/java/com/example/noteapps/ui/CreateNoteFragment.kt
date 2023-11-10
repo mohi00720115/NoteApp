9dd2638dafa54e22567650ac47765d208ec89f3e
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-10 16:08:08 +0330

    Create & add LocalBroadcastManager in CreateNoteFragment.kt

--- app/src/main/java/com/example/noteapps/ui/CreateNoteFragment.kt
+++ app/src/main/java/com/example/noteapps/ui/CreateNoteFragment.kt
@@ -1,14 +1,18 @@
 package com.example.noteapps.ui
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.noteapps.R
@@ -30,13 +34,17 @@ class CreateNoteFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_create_note, container, false)
     }
 
+    @SuppressLint("SimpleDateFormat")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DataBindingUtil.bind(view)!!
         navController = findNavController()
         with(binding) {
+            LocalBroadcastManager.getInstance(requireContext())
+                .registerReceiver(broadcastReceiver, IntentFilter("bottom_sheet_action"))
             val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
             currentDate = sdf.format(Date())
+            colorView.setBackgroundColor(Color.parseColor(selectedColor))
             tvDateTime.text = currentDate
             imgDone.setOnClickListener {
                 saveNote()
@@ -84,8 +92,47 @@ class CreateNoteFragment : BaseFragment() {
 
     private val broadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(p0: Context?, p1: Intent?) {
-            var actionColor = p1!!.getStringExtra("actionColor")
+            val actionColor = p1!!.getStringExtra("actionColor")
+
+            when (actionColor!!) {
+                "Blue" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+
+                "Yellow" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+
+                "Purple" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+
+                "Green" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+
+                "Orange" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+
+                "Black" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+
+                else -> {}
+            }
         }
+    }
+
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(requireContext()).unregisterReceiver(broadcastReceiver)
+        super.onDestroy()
     }
 
     private fun showToast(toastText: String) = Toast.makeText(requireContext(), toastText, Toast.LENGTH_SHORT).show()
