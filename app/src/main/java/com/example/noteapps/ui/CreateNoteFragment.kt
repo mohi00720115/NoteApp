68e6576cbf4ae60211a9c8251e80ec3bcfc342b2
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-11 14:52:17 +0330

    Creating functions to access permissions and storage

--- app/src/main/java/com/example/noteapps/ui/CreateNoteFragment.kt
+++ app/src/main/java/com/example/noteapps/ui/CreateNoteFragment.kt
@@ -1,12 +1,16 @@
 package com.example.noteapps.ui
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,15 +24,22 @@ import com.example.noteapps.databinding.FragmentCreateNoteBinding
 import com.example.noteapps.local.db.NotesDatabase
 import com.example.noteapps.local.entity.Notes
 import kotlinx.coroutines.launch
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 import java.text.SimpleDateFormat
 import java.util.Date
 
 
-class CreateNoteFragment : BaseFragment() {
+class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, EasyPermissions.RationaleCallbacks {
     private lateinit var binding: FragmentCreateNoteBinding
     private lateinit var navController: NavController
     private var currentDate: String? = null
     var selectedColor = "#171C26"
+    private var READ_STORAGE_PERM = 123
+    private var REQUEST_CODE_IMAGE = 456
+    private var selectedImagePath = ""
+    private var webLink = ""
+    private var noteId = -1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_create_note, container, false)
@@ -93,7 +104,7 @@ class CreateNoteFragment : BaseFragment() {
 
     private val broadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(p0: Context?, p1: Intent?) {
-            val actionColor = p1!!.getStringExtra("actionColor")
+            val actionColor = p1!!.getStringExtra("action")
 
             when (actionColor!!) {
                 "Blue" -> {
@@ -126,6 +137,10 @@ class CreateNoteFragment : BaseFragment() {
                     binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
                 }
 
+                "Image" -> {
+                    readStorageTask()
+                }
+
                 else -> {}
             }
         }
@@ -137,4 +152,82 @@ class CreateNoteFragment : BaseFragment() {
     }
 
     private fun showToast(toastText: String) = Toast.makeText(requireContext(), toastText, Toast.LENGTH_SHORT).show()
+
+    private fun hasReadStoragePerm(): Boolean {
+        return EasyPermissions.hasPermissions(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
+
+    private fun hasWriteStoragePerm(): Boolean {
+        return EasyPermissions.hasPermissions(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+
+    /**
+     * میگه اگر برناممون پرمیژن داشت که بیاد یه کاری بکنه اما اگر نداشت بیاد پرمیژن رو درخواست کنه
+     */
+    private fun readStorageTask() {
+        if (hasReadStoragePerm()) {
+            showToast("Permission Granted")
+            pickImageFromGallery()
+        } else {
+            EasyPermissions.requestPermissions(
+                requireActivity(),
+                getString(R.string.storage_permission_text),
+                READ_STORAGE_PERM,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(intent, REQUEST_CODE_IMAGE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK){
+            if (data != null){
+                var selectedImageUrl = data.data
+                if (selectedImageUrl != null){
+                    try {
+                        var inputStream = requireActivity().contentResolver.openInputStream(selectedImageUrl)
+                        var bitmap = BitmapFactory.decodeStream(inputStream)
+                        binding.imgNote.setImageBitmap(bitmap)
+                        binding.imgNote.visibility = View.VISIBLE
+
+//                        binding.ll1.visibility = View.VISIBLE
+//                        selectedImagePath = getPathFromUri(selectedImageUrl)!!
+                    }catch (e:Exception){
+                        showToast(e.message!!)
+                    }
+
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, requireActivity())
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(requireActivity(), perms)) {
+            AppSettingsDialog.Builder(requireActivity()).build().show()
+        }
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
+        TODO("Not yet implemented")
+    }
 }
