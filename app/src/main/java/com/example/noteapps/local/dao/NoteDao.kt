b98771e0b7bd487884af2bb7b22b44ca1bd3e51a
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-15 20:38:09 +0330

    create deleteSpecificNote fun

--- app/src/main/java/com/example/noteapps/local/dao/NoteDao.kt
+++ app/src/main/java/com/example/noteapps/local/dao/NoteDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
+import androidx.room.Update
 import com.example.noteapps.local.entity.Notes
 
 @Dao
@@ -20,4 +21,10 @@ interface NoteDao {
 
     @Delete
     suspend fun deleteNote(note: Notes)
+
+    @Query("DELETE FROM Notes WHERE id =:id")
+    suspend fun deleteSpecificNote(id: Int)
+
+    @Update
+    suspend fun updateNote(note: Notes)
 }
