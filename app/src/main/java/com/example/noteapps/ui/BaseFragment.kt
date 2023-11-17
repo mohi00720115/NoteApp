cdd6dfebdd86ff753ce0def7320ee580974a068c
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-17 23:37:26 +0330

    Hilt annotation

--- app/src/main/java/com/example/noteapps/ui/BaseFragment.kt
+++ app/src/main/java/com/example/noteapps/ui/BaseFragment.kt
@@ -2,11 +2,13 @@ package com.example.noteapps.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlin.coroutines.CoroutineContext
 
+@AndroidEntryPoint
 abstract class BaseFragment : Fragment(), CoroutineScope {
     private lateinit var job: Job
     override val coroutineContext: CoroutineContext
