760f55c77d1562c49d3b8338b397773252534eac
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-11-19 13:15:13 +0330

    add string & create const

---
+++ app/src/main/java/com/example/noteapps/util/Const.kt
@@ -0,0 +1,9 @@
+package com.example.noteapps.util
+
+object Const {
+    //NoteBottomSheetFragment
+    const val ACTION = "action"
+    const val SELECTED_COLOR = "selectedColor"
+    const val BOTTOM_SHEET_ACTION = "bottom_sheet_action"
+
+}
